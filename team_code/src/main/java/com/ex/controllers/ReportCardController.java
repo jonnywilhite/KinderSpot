6bf50bb64433f806fb410a673b74656256c5b769
Jonathan Wilhite <jonnygwilhite@gmail.com>
2016-08-06 14:36:03 -0400

    teacher can get report card entries from students in their class

--- team_code/src/main/java/com/ex/controllers/ReportCardController.java
+++ team_code/src/main/java/com/ex/controllers/ReportCardController.java
@@ -1,6 +1,9 @@
 package com.ex.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +21,11 @@ public class ReportCardController {
 	@RequestMapping(value="report-cards", method=RequestMethod.POST)
 	public ReportCard createReportCardEntry(@RequestBody ReportCard rc) {
 		return service.createReportCardEntry(rc);
+	}
+	
+	@RequestMapping(value="{teacherId}/report-cards", method=RequestMethod.GET)
+	public List<ReportCard> getAllReportCardsByTeacher(@PathVariable int teacherId) {
+		return service.getAllReportCardsByTeacher(teacherId);
 	}
 
 }
