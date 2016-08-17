642067486a3114fb76770cb12eb651d1c28aa8a1
hien1991 <hien_nguyen_91@yahoo.com>
2016-08-17 09:26:39 -0400

    removed random from slideshows

--- team_code/src/main/webapp/resources/pages/javascript/controllers/PhotosModalCtrl.js
+++ team_code/src/main/webapp/resources/pages/javascript/controllers/PhotosModalCtrl.js
@@ -69,40 +69,6 @@ angular.module('myApp').controller('PhotosModalCtrl', ['$scope', '$uibModalInsta
 			});
 		}
 	}
-
-	  // Randomize logic below
-
-	  function assignNewIndexesToSlides(indexes) {
-	    for (var i = 0, l = slides.length; i < l; i++) {
-	      slides[i].id = indexes.pop();
-	    }
-	  }
-
-	  function generateIndexesArray() {
-	    var indexes = [];
-	    for (var i = 0; i < currIndex; ++i) {
-	      indexes[i] = i;
-	    }
-	    return shuffle(indexes);
-	  }
-
-	  // http://stackoverflow.com/questions/962802#962890
-	  function shuffle(array) {
-	    var tmp, current, top = array.length;
-
-	    if (top) {
-	      while (--top) {
-	        current = Math.floor(Math.random() * (top + 1));
-	        tmp = array[current];
-	        array[current] = array[top];
-	        array[top] = tmp;
-	      }
-	    }
-
-	    return array;
-	  }
-	
-	
 	
 	
 	
